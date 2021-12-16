74b02d95f9c459e5f9c1dcd11becade79e5e9032
Leonardo Parga <leonardoparga@gmail.com>
2021-12-15 23:06:22 -0300

    2021 Days 5 and 6

---
+++ AdventOfCode/2021/Day5.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode._2021
+{
+    public class Day5 : Day, IChallenge
+    {
+        public IEnumerable<string> Run()
+        {
+            var rgx = new Regex(@"(\d+),(\d+) -> (\d+),(\d+)");
+            var matches = rgx.Matches(RawInput);
+            var values = matches.Select(m => (x1: int.Parse(m.Groups[1].Value), y1: int.Parse(m.Groups[2].Value), x2: int.Parse(m.Groups[3].Value), y2: int.Parse(m.Groups[4].Value))).ToList();
+
+            yield return ResultString(GetNumOfPointsWhereLinesOverlap(values, onlyHorizontalAndVertical: true));
+            yield return ResultString(GetNumOfPointsWhereLinesOverlap(values));
+        }
+
+        private int GetNumOfPointsWhereLinesOverlap(List<(int x1, int y1, int x2, int y2)> values, bool onlyHorizontalAndVertical = false)
+        {
+            var map = new List<(int, int)>();
+
+            foreach (var value in values)
+            {
+                if (onlyHorizontalAndVertical && value.x1 != value.x2 && value.y1 != value.y2)
+                    continue;
+
+                var current = (value.x1, value.y1);
+                var target = (value.x2, value.y2);
+
+                while (current != target)
+                {
+                    map.Add(current);
+
+                    // get next point in line, moving towards target
+                    current = (x1: current.x1 == value.x2 ? current.x1 : current.x1 > value.x2 ? current.x1 - 1 : current.x1 + 1,
+                               y1: current.y1 == value.y2 ? current.y1 : current.y1 > value.y2 ? current.y1 - 1 : current.y1 + 1);
+                }
+
+                map.Add(target);
+            }
+
+            return map.GroupBy(x => x).Where(x => x.Count() > 1).Count();
+        }
+    }
+}
---
+++ AdventOfCode/2021/Day6.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode._2021
+{
+    public class Day6 : Day, IChallenge
+    {
+        private const int spawnTimer = 6;
+        private const int newFishExtraTime = 2;
+
+        private Dictionary<(int fishTimer, int currentDay), ulong> SolvedFishDictionary = new Dictionary<(int fishTimer, int currentDay), ulong>();
+
+        public IEnumerable<string> Run()
+        {
+            var initialFishes = RawInput.Split(',').Select(x => int.Parse(x)).ToList();
+
+            yield return ResultString(GetNumberOfFishAfterDays(initialFishes, days: 80));
+            yield return ResultString(GetNumberOfFishAfterDays(initialFishes, days: 256));
+        }
+
+        private ulong GetNumberOfFishAfterDays(List<int> initialFishes, int days)
+        {
+            ulong fishSpawned = 0;
+            SolvedFishDictionary = new Dictionary<(int fishTimer, int currentDay), ulong>();
+
+            foreach (var fishTimer in initialFishes)
+                fishSpawned += GetNumOfFishesSpawnedInDays(fishTimer, days);
+
+            return fishSpawned + (ulong)initialFishes.Count;
+        }
+
+        private ulong GetNumOfFishesSpawnedInDays(int fishTimer, int days, int currentDay = 1)
+        {
+            ulong numOfFishSpawnedByOriginalFish = 0;
+            ulong numOfRecursiveFishesSpawned = 0;
+            var originalTimer = fishTimer;
+            var originalDay = currentDay;
+
+            // check if a fish with these parameters has been solved before
+            if (SolvedFishDictionary.TryGetValue((fishTimer, currentDay), out ulong totalFishes))
+                return totalFishes;
+
+            while (currentDay <= days)
+            {
+                var previousTimer = fishTimer;
+                fishTimer = (fishTimer - 1) < 0 ? spawnTimer : fishTimer - 1;
+
+                if (fishTimer == spawnTimer && previousTimer == 0) // if the timer resets back to the spawn timer, spawn a new fish
+                {
+                    numOfFishSpawnedByOriginalFish++;
+                    numOfRecursiveFishesSpawned += GetNumOfFishesSpawnedInDays(spawnTimer + newFishExtraTime, days, currentDay + 1); // plus one because the day the fish spawns, the timer doesn't decrease
+                }
+
+                currentDay++;
+            }
+
+            totalFishes = numOfFishSpawnedByOriginalFish + numOfRecursiveFishesSpawned;
+            SolvedFishDictionary.Add((originalTimer, originalDay), totalFishes);
+            return totalFishes;
+        }
+    }
+}
